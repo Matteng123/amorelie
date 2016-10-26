3ce6eec3397ef5348da785b46f6b9a6150713955
danielkremin <design@kremin.de>
2016-10-26 02:19:29 +0200

    Update PHP

--- public/nackte-tatsachen/index.php
+++ public/nackte-tatsachen/index.php
@@ -32,22 +32,22 @@
 		<meta property="og:title"              content="36 nackte Tatsachen" />
 		<?php if($_GET['section'] == 'fact1') : ?>
 		<meta property="og:description"        content="In Socken verlocken" />
-		<?php endif ?>
+		<?php endif; ?>
 		<?php if($_GET['section'] == 'fact2') : ?>
 		<meta property="og:description"        content="Strumpfband vs. Schrumpfband" />
-		<?php endif ?>
+		<?php endif; ?>
 		<?php if($_GET['section'] == 'fact3') : ?>
 		<meta property="og:description"        content="Kein Witz!" />
-		<?php endif ?>
+		<?php endif; ?>
 		<?php if($_GET['section'] == 'fact4') : ?>
 		<meta property="og:description"        content="Harte Tatsachen" />
-		<?php endif ?>
+		<?php endif; ?>
 		<?php if($_GET['section'] == 'fact5') : ?>
 		<meta property="og:description"        content="Gib Gummi!" />
-		<?php endif ?>
+		<?php endif; ?>
 		<?php if($_GET['section'] == 'fact6') : ?>
 		<meta property="og:description"        content="So hot right now" />
-		<?php endif ?>
+		<?php endif; ?>
 
     <style>
       body {
