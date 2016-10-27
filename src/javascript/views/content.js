609e8b4843c08e1f1de77b5624e699c08e61a32a
danielkremin <design@kremin.de>
2016-10-27 21:27:23 +0200

    #1 fixed

--- src/javascript/views/content.js
+++ src/javascript/views/content.js
@@ -154,14 +154,20 @@ let Content = SMContent.extend({
 
 	handleChange9: function(e){
 		TweenMax.set(this.output9, {opacity:0});
-		var targetAge = 71;
-		if(e.target.value >= targetAge) {
-			targetAge = targetAge+10
+		var startAge = 16;
+		var maxAge = Number(e.target.value) > 71 ? 71 : e.target.value;
+		if(maxAge <= 16){
+			maxAge = 16;
 		}
-		if(e.target.value >= targetAge) {
-			targetAge = 101;
+
+		var days = (maxAge-startAge)*0.45;
+		if(days < 10){
+			days = this.decimalAdjust('round', days, -1)
+		} else {
+			days = Math.round(days);
 		}
-		this.output9.setAttribute("value", Math.round((targetAge-e.target.value)*0.45));
+
+		this.output9.setAttribute("value", days);
 		TweenMax.to('#largepointer', 0.5, {rotation:360, transformOrigin:'right bottom', onComplete:function(){
 			TweenMax.set('#largepointer', {rotation:0, transformOrigin:'right bottom'});
 		}});
@@ -174,7 +180,26 @@ let Content = SMContent.extend({
 		if(e.target.getAttribute("value") == '??'){
 			e.target.setAttribute("value", "");
 		}
-	}
+	},
+	decimalAdjust: function (type, value, exp) {
+	 // If the exp is undefined or zero...
+	 if (typeof exp === 'undefined' || +exp === 0) {
+		 return Math[type](value);
+	 }
+	 value = +value;
+	 exp = +exp;
+	 // If the value is not a number or the exp is not an integer...
+	 if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+		 return NaN;
+	 }
+	 // Shift
+	 value = value.toString().split('e');
+	 value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+	 // Shift back
+	 value = value.toString().split('e');
+	 return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+ }
+
 
 });
 
